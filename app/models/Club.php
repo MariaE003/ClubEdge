651195c6da51ac6fdb6edcc8ab0bf5814ce81cfb
Mouadelhiloufi <elmouad940@gmail.com>
2026-01-20 16:12:41 +0100

    ajouter methode de validation

---
+++ app/models/Club.php
@@ -0,0 +1,77 @@
+<?php
+
+    class Club{
+       
+
+    private $db;
+
+    public function __construct(){
+    $this->db=Database::getInstance()->getConnection();
+    }
+    
+
+    public function countMembers(){
+        $sql_prepare="SELECT cardinality(members) as total from clubs where id=?";
+        $sql=$this->db->prepare($sql_prepare);
+        $sql->execute([$this->id]);
+        $result=$sql->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+
+    public function isStudentInClub($user_id){
+    $sql_prepare="SELECT count(*) where ? = ANY(members)";
+    $sql=$this->db->prepare($sql_prepare);
+    $sql->execute([$user_id]);
+    $result=$sql->fetchColumn();
+    return $result>0;
+    }
+
+
+
+
+    public function canJoin(){
+        $count_members=$this->countMembers();
+        if($count_members>=8){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+
+    public function joinClub($user_id){
+        try{
+
+            $this->db->beginTransaction();
+
+            $currentCount=$this->countMembers();
+            
+            $sql_prepare="UPDATE clubs SET members =array_append(members,?) where id=?";
+            $sql=$this->db->prepare($sql_prepare);
+            $sql->execute([$user_id,$this->id]);
+
+            if($currentCount==0){
+                $sql_pre="UPDATE clubs set president_id=? where id=?";
+                $sql=$this->db->prepare($sql_pre);
+                $sql->execute([$user_id,$this->id]);
+
+                $prepareRole="UPDATE users set role='president' where id=?";
+                $sqlRole=$this->db->prepare($prepareRole);
+                $sqlRole->execute([$user_id]);
+            }
+            $this->db->commit();
+            
+        }catch(Exception $e){
+            $this->db->rollBack();
+            return $e->getMessage();
+            
+        }
+    }
+
+
+
+
+    }
+
+?>
--- config/config.php
+++ config/config.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'clubedge');
 define('DB_USER', 'postgres');
-define('DB_PASS', ',ouhsinerouqki');
+define('DB_PASS', 'mouad');
 define('DB_PORT', '5432');
