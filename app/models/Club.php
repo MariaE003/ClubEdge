755f6010da18e327a2c69796ac80b72bd88dc530
MariaE003 <elgotbymaria@gmail.com>
2026-01-20 14:27:52 +0100

    ajouter le model et repository des fonctionnalite  crud du club

--- app/models/Club.php
+++ app/models/Club.php
@@ -1,4 +1,71 @@
 <?php 
 
+class Club{
+    private ?int $id;
+    private string $nom;
+    private ?string $description;
+
+    private ?int $presidentId;
+    private ?string $logo;
+    private ?string $createdAt; 
+    private array $members;
+
+    public function __construct(?int $id,string $nom,?string $description=null,?int $presidentId=null,?string $logo = null,array $members=[],?string $createdAt = null){
+    $this->id=$id;
+    $this->setNom($nom);
+    $this->description=$description;
+    $this->logo=$logo;
+    $this->presidentId=$presidentId;
+    $this->setMembers($members);
+    $this->createdAt=$createdAt;
+
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getNom(): string {
+        return $this->nom;
+    }
+    public function setNom(string $nom): void {
+        if (trim($nom)==='') {
+            throw new Exception('le nom du club est obligatoire !');
+        }
+        $this->nom = $nom;
+    }
+
+    public function getDescription(): ?string {
+        return $this->description;
+    }
+    public function setDescription(?string $description): void {
+        $this->description = $description;
+    }
+    // 
+    public function getPresidentId(): ?int{ 
+        return $this->presidentId; 
+    }
+    public function setPresidentId(?int $presidentId){
+        $this->presidentId = $presidentId;
+    }
+
+    public function getMembers(): array{ 
+        return $this->members; 
+    }
+
+    public function getLogo(): ?string{ 
+        return $this->logo;  
+    }
+    public function setLogo(?string $logo): void {
+        $this->logo = $logo;
+    }
+
+    public function getCreatedAt(): ?string {
+        return $this->createdAt; 
+    }
+
+
+
+}
 
 ?>
--- app/repositories/ClubRepository.php
+++ app/repositories/ClubRepository.php
@@ -1,7 +1,34 @@
 <?php
-// class ClubRepository{
-//     public function 
 
-// }
+class ClubRepository{
+    private PDO $db;
+    public function __construct(PDO $db){
+        $this->db=$db;
+    }
+    // ajouter un club
+    public function addClub(Club $club){
+        $req=$this->db->prepare("INSERT into clubs(name,description,president_id,members,logo) values(?,?,?,?,?)");
+        // getMember
+        return $req->execute([$club->getNom(),$club->getDescription(),$club->getPresidentId(),'{'. implode(',',$club->getMembers()).'}',$club->getLogo()]);
+    }
+    // affichier tous les club
+    public function findAllClubs(){
+        $req=$this->db->prepare("SELECT * from clubs");
+        $req->execute();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+    // supprimer un clubs
+    public function removeClub(int $id){
+    $req=$this->db->prepare("DELETE from clubs where id=?");
+        return $req->execute([$id]);
+    }
+    // modifier un club
+    public function updateClub(Club $club){
+    $req=$this->db->prepare("UPDATE clubs set name=?,description=?,members=?,logo=? where id=?");
+    // member
+        return $req->execute([$club->getNom() ,$club->getDescription(),'{'. implode(',',$club->getMembers()).'}',$club->getLogo(),$club->getId()]);
+    }
 
+    // rechercher sur un club ?
+}
 ?>
